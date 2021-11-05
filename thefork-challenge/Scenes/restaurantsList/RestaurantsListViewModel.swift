c2ee0a346f95956c3de369a77c904c9e0303b79d
fermenendez77 <fermenendez77@gmail.com>
2021-11-05 17:14:18 +0100

    Added viewModel for restaurants lists, Binding and performing the data fetching

--- thefork-challenge/Scenes/restaurantsList/RestaurantsListViewModel.swift
+++ thefork-challenge/Scenes/restaurantsList/RestaurantsListViewModel.swift
@@ -12,19 +12,24 @@ class RestaurantsListViewModel {
     //Dependencies
     let dataFetcher : RestaurantsListDataFetcher
     
-    //Data
+    //Data Binding
     var restaurants : Binding<[Restaurant]> = Binding([])
     var hasError : Binding<Bool> = Binding(false)
+    var isLoading : Binding<Bool> = Binding(false)
+    
+    var dataCount : Int { restaurants.value.count }
     
     public init(dataFetcher : RestaurantsListDataFetcher = RestaurantsListDataFetcherService()) {
         self.dataFetcher = dataFetcher
     }
     
     public func fetchData() {
+        isLoading.value = true
         dataFetcher.fetchData { [weak self] result in
             guard let self = self else {
                 return
             }
+            self.isLoading.value = false
             switch result {
             case .success(let data):
                 self.restaurants.value = data
--- thefork-challenge/Scenes/restaurantsList/ViewController.swift
+++ thefork-challenge/Scenes/restaurantsList/ViewController.swift
@@ -10,10 +10,34 @@ import UIKit
 class ViewController: UIViewController {
     
     var collectionView : UICollectionView!
+    var viewModel = RestaurantsListViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
+        configureDataBinding()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewModel.fetchData()
+    }
+    
+    func configureDataBinding() {
+        viewModel.restaurants.bind { [weak self] _ in
+            self?.collectionView.reloadData()
+        }
+        
+        viewModel.isLoading.bind { [weak self] isLoading in
+            guard let self = self else {
+                return
+            }
+            if isLoading {
+                self.present(self.loadingView, animated: true)
+            } else {
+                self.loadingView.dismiss(animated: true)
+            }
+        }
     }
 
     func configureView() {
@@ -22,7 +46,59 @@ class ViewController: UIViewController {
                                           collectionViewLayout: flowLayout)
         self.view.addSubview(collectionView)
         collectionView.attachAnchors(to: self.view)
-        collectionView.backgroundColor = .blue
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellID")
+    }
+    
+    let loadingView : UIAlertController = {
+        let alert = UIAlertController(title: nil,
+                                      message: "Please wait...",
+                                      preferredStyle: .alert)
+
+        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10,
+                                                                     y: 5,
+                                                                     width: 50,
+                                                                     height: 50))
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.style = UIActivityIndicatorView.Style.gray
+        loadingIndicator.startAnimating()
+        alert.view.addSubview(loadingIndicator)
+        return alert
+    }()
+}
+
+extension ViewController : UICollectionViewDelegate {
+    
+    
+}
+
+extension ViewController : UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.bounds.width, height: 70.0)
     }
 }
+
+extension ViewController : UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath)
+        cell.backgroundColor = .green
+        return cell;
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return viewModel.dataCount
+    }
+}
+
+
 
